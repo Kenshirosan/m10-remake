5bbb57cf700e4bf82a3d1aacf458ede687852f8f
laurent <l.neveux@icloud.com>
2021-07-19 10:01:00 +0200

    init m10

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import logo from "../assets/img/logo.svg";
+
+const Header = () => {
+  console.log(logo);
+  return (
+    <header className="container">
+      <a href="/">
+        <img src={logo} alt="logo de mon portfolio" /> John Doe
+      </a>
+      <nav>
+        <a href="/">
+          <i className="fa fa-home" /> Accueil
+        </a>
+        <a href="#">
+          <i className="fa fa-folder-open" /> Réalisations
+        </a>
+        <a href="#">
+          <i className="fa fa-envelope" /> Contact
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

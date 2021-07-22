c7062d37384cf9fbb0394c99d955dc3b688665bd
laurent <l.neveux@icloud.com>
2021-07-22 10:02:36 +0200

    Next : Create a Users List

---
+++ src/components/userslist/UsersList.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+/**
+ * Créer un state avec une propriété users et un isLoading
+ *
+ * Récupérer une liste d'utilisateur avec fetch et l'api typicode, mettre les users dans le state
+ *
+ * Utilisez un constructor et componentDidMount()
+ *
+ * Créez un composant user qui acceptera un prop user
+ */
+class UsersList extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Users</h2>
+      </div>
+    );
+  }
+}
+
+export default UsersList;

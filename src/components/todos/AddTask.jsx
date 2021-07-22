61f9e5f1af2adb3fa2662cef2565b458d7c4ac05
laurent <l.neveux@icloud.com>
2021-07-22 12:52:48 +0200

    finish todolist

--- src/components/todos/AddTask.jsx
+++ src/components/todos/AddTask.jsx
@@ -1,27 +1,59 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AddTask extends Component {
-  render() {
-    return (
-      <form>
-        <fieldset>
-          <ul>
-            <li>
-              <legend>Ajoutez une tâche </legend>
-              <input type="text" name="title" />
-              <button style={btnStyle} type="submit">
-                Créer une tâche
-              </button>
-            </li>
-          </ul>
-        </fieldset>
-      </form>
-    );
-  }
+    state = {
+        title: '',
+    };
+
+    recordInputValue(e) {
+        this.setState({ title: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        // Créer une tâche
+
+        let task = {
+            id: Math.random().toString(16),
+            title: this.state.title,
+            done: false,
+        };
+
+        this.props.createTask(task);
+        e.target.reset();
+    }
+
+    render() {
+        return (
+            <form onSubmit={e => this.handleSubmit(e)}>
+                <fieldset>
+                    <ul>
+                        <li>
+                            <legend>Ajoutez une tâche </legend>
+                            <input
+                                onChange={this.recordInputValue.bind(this)}
+                                type="text"
+                                name="title"
+                            />
+                            <button style={btnStyle} type="submit">
+                                Créer une tâche
+                            </button>
+                        </li>
+                    </ul>
+                </fieldset>
+            </form>
+        );
+    }
 }
 
+AddTask.propTypes = {
+    createTask: PropTypes.func.isRequired,
+};
+
 const btnStyle = {
-  marginLeft: "1em",
+    marginLeft: '1em',
 };
 
 export default AddTask;
--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -1,124 +1,93 @@
-import { Component, Fragment } from "react";
-import Loader from "../blog/Loader";
-import Task from "./Task";
-import AddTask from "./AddTask";
-import data from "./data";
-/*
- *
- * CRUD
- * CREATE
- * READ
- * UPDATE
- * DELETE
- *
- * RESTFul API
- *
- * INDEX => Récupérer les données et les afficher. (maybe...)
- * CREATE => Montre un formulaire pour créer une resource.
- * STORE => Créer une resource : Besoin des données du formulaire.
- * SHOW => Montre une resource : On a besoin d'identifier la resource : il faut un identifiant unique.
- * EDIT => Montre un formulaire pré-rempli : On a besoin d'un identifiant unique, on a besoin de l'objet à mettre à jour.
- * UPDATE => Met à jour la resource : Besoin des données du formulaire et d'un identifiant unique.
- * DESTROY => Efface la resource : besoin d'un identifiant unique.
- *
- */
+import { Component, Fragment } from 'react';
+import Loader from '../blog/Loader';
+import Task from './Task';
+import AddTask from './AddTask';
+import data from './data';
 
 class TodoList extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      isLoading: true,
-      tasks: [],
-    };
+        this.state = {
+            isLoading: true,
+            tasks: [],
+        };
 
-    this.updateTask = this.updateTask.bind(this);
-    this.deleteTask = this.deleteTask.bind(this);
-  }
-
-  // Méthode du cycle de vie du composant
-  // est appelée automatiquement après le constructeur
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ tasks: data });
-
-      this.setState({ isLoading: false });
-    }, 500);
-  }
-
-  deleteTask(id) {
-    if (window.confirm("Etes vous sur de vouloir effacer cette tâche ?")) {
-      //
-      this.setState({
-        tasks: this.state.tasks.filter((task) => task.id !== id),
-      });
+        this.updateTask = this.updateTask.bind(this);
+        this.deleteTask = this.deleteTask.bind(this);
+        this.createTask = this.createTask.bind(this);
     }
-  }
 
-  updateTask(updatedTask) {
-    // Trouver la tache dans le state : On la reçu en paramètre
-    // Mettre à jour la tâche : La propriété done doit prendre la valeur inverse de ce qu'elle est maintenant
-    updatedTask.done = !updatedTask.done;
+    createTask(task) {
+        this.setState({ tasks: [task, ...this.state.tasks] });
+    }
 
-    // map retourne un tableau modifié selon une condition,
-    // Ce tableau contient le même nombre d'éléments que le tableau original
-    //
-    // Mettre a jour le tableau contacts
-    // Avec map : si on trouve sur un contact la même id que l'on a dans updateContact, on remplace l'objet.
-    // Sinon on retourne l'objet en place sans modifications.
-    let newTasks = this.state.tasks.map((task) => {
-      return updatedTask.id === task.id ? updatedTask : task;
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ tasks: data });
 
-      // On compare l'id de la tâche qu'on a mis à jour avec les id des tâches du state
-      // if (updatedTask.id === task.id) {
-      //   return updatedTask;
-      // }
-      // return task;
-    });
+            this.setState({ isLoading: false });
+        }, 500);
+    }
 
-    // Mettre à jour le state
-    this.setState({ tasks: newTasks });
-  }
+    deleteTask(id) {
+        if (window.confirm('Etes vous sur de vouloir effacer cette tâche ?')) {
+            //
+            this.setState({
+                tasks: this.state.tasks.filter(task => task.id !== id),
+            });
+        }
+    }
 
-  render() {
-    const { isLoading, tasks } = this.state;
+    updateTask(updatedTask) {
+        updatedTask.done = !updatedTask.done;
 
-    return (
-      <section className="container">
-        <h2>TodoList</h2>
+        this.setState({
+            tasks: this.state.tasks.map(task =>
+                updatedTask.id === task.id ? updatedTask : task
+            ),
+        });
+    }
 
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <Fragment>
-            <AddTask />
-            <table>
-              <thead>
-                <tr>
-                  <th>id</th>
-                  <th>Title</th>
-                  <th>Done</th>
-                  <th>Action</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/*  Afficher data */}
-                {tasks.map((task) => (
-                  <Task
-                    key={task.id}
-                    task={task}
-                    updateTask={this.updateTask}
-                    deleteTask={this.deleteTask}
-                  />
-                ))}
-              </tbody>
-            </table>
-          </Fragment>
-        )}
-      </section>
-    );
-  }
+    render() {
+        const { isLoading, tasks } = this.state;
+
+        return (
+            <section className="container">
+                <h2>TodoList</h2>
+
+                {isLoading ? (
+                    <Loader />
+                ) : (
+                    <Fragment>
+                        <AddTask createTask={this.createTask} />
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>id</th>
+                                    <th>Title</th>
+                                    <th>Done</th>
+                                    <th>Action</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/*  Afficher data */}
+                                {tasks.map(task => (
+                                    <Task
+                                        key={task.id}
+                                        task={task}
+                                        updateTask={this.updateTask}
+                                        deleteTask={this.deleteTask}
+                                    />
+                                ))}
+                            </tbody>
+                        </table>
+                    </Fragment>
+                )}
+            </section>
+        );
+    }
 }
 
 export default TodoList;

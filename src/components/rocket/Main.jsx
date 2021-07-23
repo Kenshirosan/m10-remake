64961adfa1c0e1f9f5060806d3aac6187e00b354
laurent <l.neveux@icloud.com>
2021-07-23 11:32:05 +0200

    add images and some empty methods

--- src/components/rocket/Main.jsx
+++ src/components/rocket/Main.jsx
@@ -3,8 +3,17 @@ import Aside from './Aside';
 
 import moon from '../../rocket-assets/images/fullmoon.png';
 import launchingRamp from '../../rocket-assets/images/launching-ramp.png';
+import firingBtn from '../../rocket-assets/images/firing-button.png';
+import cancelBtn from '../../rocket-assets/images/cancel-button.png';
+import rocket1 from '../../rocket-assets/images/rocket1.png';
 
 class Main extends Component {
+    state = {};
+
+    startRocket() {}
+
+    stopRocket() {}
+
     render() {
         return (
             <main>
@@ -17,15 +26,17 @@ class Main extends Component {
                 />
                 <img
                     id="firing-button"
-                    src="images/firing-button.png"
+                    src={firingBtn}
                     alt="Firing button"
+                    onClick={this.startRocket}
                 />
                 <img
                     id="cancel-button"
-                    src="images/cancel-button.png"
+                    src={cancelBtn}
                     alt="Cancel button"
+                    onClick={this.stopRocket}
                 />
-                <img id="rocket" src="images/rocket1.png" alt="rocket" />
+                <img id="rocket" src={rocket1} alt="rocket" />
             </main>
         );
     }

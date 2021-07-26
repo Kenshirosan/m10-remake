943c7d7418b1e9fddc24a1e68205a3c0e7388865
laurent <l.neveux@icloud.com>
2021-07-26 12:46:35 +0200

    Start Accordion

--- src/components/accordeon-basics/Article.jsx
+++ src/components/accordeon-basics/Article.jsx
@@ -1,6 +1,6 @@
 // 1. Accepter article en prop
 // 2. Pour afficher l'article qui arrive en prop:
-//      Utiliser la balise article,
+//      Utiliser la balise <article>,
 //      une balise h2 pour le titre avec un lien a href dans le h2,
 //      et une balise p.
 // 3. Ensuite, transférer la balise p dans les composant Accordion.
